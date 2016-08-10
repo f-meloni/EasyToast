4fc6a6232d0761a7c75b02124d45887b29610895
Franco Meloni <franco.meloni@xorovo.com>
2016-08-10 15:07:32 +0200

    More improvements to Example project

--- Example/EasyToast/ViewController.swift
+++ Example/EasyToast/ViewController.swift
@@ -13,21 +13,10 @@ class ViewController: UIViewController {
     private lazy var button : UIButton = {
         let button = UIButton(type: .System)
         
-        button.frame = CGRectMake(0, 20, CGRectGetWidth(self.view.frame), 50)
+        button.frame = CGRectMake(0, 50, CGRectGetWidth(self.view.frame), 50)
         button.titleLabel?.font = UIFont.systemFontOfSize(21)
         button.setTitle("Send Toasts", forState: .Normal)
         button.addTarget(self, action: #selector(showToast), forControlEvents: .TouchUpInside)
-        
-        return button
-    }()
-    
-    private lazy var button2 : UIButton = {
-        let button = UIButton(type: .System)
-        
-        button.frame = CGRectMake(0, 80, CGRectGetWidth(self.view.frame), 50)
-        button.titleLabel?.font = UIFont.systemFontOfSize(21)
-        button.setTitle("Present Modal", forState: .Normal)
-        button.addTarget(self, action: #selector(pushTestVC), forControlEvents: .TouchUpInside)
         
         return button
     }()
@@ -36,7 +25,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.addSubview(self.button)
-        self.view.addSubview(self.button2)
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,22 +40,13 @@ class ViewController: UIViewController {
         self.view.showToast("Toast", position: .Bottom, popTime: 2, dismissOnTap: false)
         self.view.showToast("Dismiss on tap toast", position: .Bottom, popTime: kToastNoPopupTime, dismissOnTap: true, bgColor: UIColor.redColor().colorWithAlphaComponent(0.7), textColor: UIColor.whiteColor(), font: UIFont.systemFontOfSize(19))
         self.view.showToast("Long Text Toast:\n" +
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae elit non leo pellentesque feugiat. Cras nec volutpat massa, nec blandit nisi. Etiam ut hendrerit purus. Morbi accumsan, risus ut cursus finibus, quam ipsum egestas nisl, vel hendrerit massa justo nec metus. Cras pulvinar, leo eu pulvinar convallis, tellus felis laoreet massa, ac tincidunt orci massa non odio. Ut pulvinar dictum metus quis mollis. Aenean tincidunt sit amet turpis sed egestas. Morbi porta dolor neque, ut pellentesque urna semper id. Cras rhoncus consequat justo. Cras dictum enim orci, ac vestibulum enim cursus id.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae elit non leo pellentesque feugiat. Cras nec volutpat massa, nec blandit nisi. Etiam ut hendrerit purus. Morbi accumsan, risus ut cursus finibus, quam ipsum egestas nisl, vel hendrerit massa justo nec metus. Cras pulvinar, leo eu pulvinar convallis, tellus felis laoreet massa, ac tincidunt orci massa non odio. Ut pulvinar dictum metus quis mollis. Aenean tincidunt sit amet turpis sed egestas. Morbi porta dolor neque, ut pellentesque urna semper id. Cras rhoncus consequat justo. Cras dictum enim orci, ac vestibulum enim cursus id.",
                             position: .Bottom,
                             popTime: 5,
                             dismissOnTap: true,
                             bgColor: UIColor.redColor().colorWithAlphaComponent(0.7),
                             textColor: UIColor.whiteColor(),
                             font: UIFont.systemFontOfSize(19))
-    }
-    
-    func pushTestVC() {
-        let vc = TestVCViewController()
-        let navigationVC = UINavigationController(rootViewController: vc)
-        
-        vc.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .Plain, target: self, action: #selector(closeModal))
-        
-        self.presentViewController(navigationVC, animated: true, completion: nil)
     }
 
     func closeModal() {
