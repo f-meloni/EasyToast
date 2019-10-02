cf29f5c4d93c3c2d41e0ff70930fcfd6cd4754a9
Franco Meloni <franco.meloni91@gmail.com>
2019-10-02 11:27:23 +0100

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -4,3 +4,5 @@ let danger = Danger()
 if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
     warn("Big PR, try to keep changes smaller if you can")
 }
+
+danger.git.modifiedFiles.forEach { fail(message: "TEST", file: $0, line: 1) }
